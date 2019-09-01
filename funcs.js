d3f14b3d208d86a33a57ab63fcdf605596f66b70
Seisei-hi <46417559+Seisei-hi@users.noreply.github.com>
2019-09-01 17:11:42 +0900

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -1,35 +1,8 @@
-let scheduleSerialNumber = 0;
-let sendImgDataBinary = "";
-
-class ScheduleObject {
-    constructor(title, due, detailText, image = null, importance = null) {
-        this.title = title;
-        this.due = due;
-        this.detailText = detailText;
-        this.image = image;
-        this.importance = importance;
-        this.serialNumber = scheduleSerialNumber++;
-    }
-}
 function createSchedule() {
-    var title = document.getElementById('title').innerHTML;
-    //https://poiemaweb.com/js-date
-    var due = new Date(
-        document.getElementById('year').value,
-        document.getElementById('month').value,
-        document.getElementById('day').value,
-        0, 0, 0, 0);
-    var detailText = document.getElementById('detail').innerHTML;
-    var importance = document.getElementById("importance").checked;
-
-    var schedule = new ScheduleObject(title, due, detailText, 0, importance);
-
-    scheduleJson = JSON.stringify(schduleArray);
-
-    applyScheduleJson(scheduleJson);
+    
 }
 function applyScheduleJson(json) {
-    var jsonArray = JSON.parse(json);
+
 }
 function sendFormData() {
     var xhr = new XMLHttpRequest();
@@ -41,8 +14,9 @@ function sendFormData() {
         }
     }
     fileReader.onload = function (event) {
-        xhr.send(`${/*title*/}$tr2$end$plitor${/*due*/}$tr2$end$plitor${/*detailText*/}$tr2$end$plitor${event.target.result}`);
+        xhr.send(`${event.target.result}`);
     }
+
     xhr.open("POST", "/post/upload", true);
     fileReader.readAsBinaryString(document.getElementById("imageInput").files[0]);
 }
