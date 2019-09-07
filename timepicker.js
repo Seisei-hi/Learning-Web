3515b06c18a7fc487f2fb23f84e5e156522912ab
Seisei-hi <46417559+Seisei-hi@users.noreply.github.com>
2019-09-07 23:52:32 +0900

    Add files via upload

--- timepicker.js
+++ timepicker.js
@@ -1,7 +1,10 @@
 class Datepicker{
     constructor() {
-        this.dayContainer = document.getElementById("datepicker-calender-day-container");
-        this.calenderMonth = document.getElementById("datepicker-calender-month");
+        this.dayContainerElement = document.getElementById("datepicker-calender-day-container");
+        this.calenderDateElement = document.getElementById("datepicker-calender-month");
+        this.number2Month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+        this.calenderMonth = new Date().getMonth();
+        
         
         //this.asd = datepicker-timepicker-slider
     
@@ -9,37 +12,41 @@ class Datepicker{
             this.clearCalender();
 
             var date = new Date();
-            date.setMonth(month);
-
-            var selectedYear = date.getFullYear();
-            var selectedMonth = date.getMonth();
             
-            this.calenderMonth.innerText = selectedMonth;
-
+            date.setMonth(month);
+            console.log(date);
+            
+            var selectedMonth = date.getMonth();
+            var selectedYear = date.getFullYear();
+            this.calenderMonth = month;
+            this.calenderDateElement.innerText = `${this.number2Month[selectedMonth]} ${selectedYear}`;
+            
             date.setFullYear(selectedYear,selectedMonth,1);
-            var thisSundayDate = date.getDate();
-
-            for (let i = 1; i < date.getDay() != 0 ; i--) {
-                date.setFullYear(selectedYear, selectedMonth, i);
+            
+            for (var thisSundayDate = 1; date.getDay() != 0 ; thisSundayDate--) {
+                date.setFullYear(selectedYear, selectedMonth, thisSundayDate);
             }
 
-            for (let i = 0; i < 42; i++) {
-                let dayDiv = document.createElement("div");
-                date.setFullYear(selectedYear, selectedMonth, thisSundayDate + i);
-
-                if(date.getMonth() != selectedMonth){
-                    dayDiv.style.color="rgb(70,70,70)";
+            for (let i = 0; i < 6; i++) {
+                let tr = document.createElement("tr");
+                for (let j = 0; j < 7; j++) {
+                    let td = document.createElement("td");
+                    date.setFullYear(selectedYear, selectedMonth, thisSundayDate +i*7+j);
+                    td.innerText=date.getDate();
+                    td.className += ` datepicker-calender-day date`;
+                    if(date.getMonth() != selectedMonth){
+                        td.style.color="rgb(70,70,70)";
+                    }
+                    tr.appendChild(td);
                 }
-
-                dayDiv.className += ` datepicker-calender-day date`;
-                dayDiv.innerText=date.getDate();
-                this.dayContainer.appendChild(dayDiv);
+                this.dayContainerElement.appendChild(tr);
             }
         }
+        
         this.clearCalender = function(){
-            var childrenLength = this.dayContainer.children.length;
-            for (let i=7; i < childrenLength; i++) {
-                this.dayContainer.children[i].remove();
+            var childrenLength = this.dayContainerElement.children.length;
+            for (let i=0; i < childrenLength; i++) {
+                this.dayContainerElement.children[0].remove(); 
             }
         }
     }
